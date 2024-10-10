3a666223b29622a64b0225195930547bd1c22175
Elin Sjögren <123662483+ElinSjogren@users.noreply.github.com>
2024-10-10 15:23:38 +0200

    basic configuration to se user and views live. Just open another tab and enter same url.

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,32 @@
+﻿//create connection at a url location
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+
+// connect to methods th at hub invokes aka recive notifications from the hub :D
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsContainer");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersContainer");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notafikation to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+};
+
+
+//start the connection
+function fulfilled() {
+    console.log("Connection to userhub was Sccessful");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+
+}
+
+
+connectionUserCount.start().then(fulfilled, rejected);
